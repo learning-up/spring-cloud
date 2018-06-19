f5dfe01aa99523337fc316f6ecd1b2655006b1a8
ZHQ <hq.zhang@yunding360.com>
2018-06-19 15:07:27 +0800

    init

--- cloud-simple-hello/src/main/java/sc/simple/hello/controller/HelloController.java
+++ cloud-simple-hello/src/main/java/sc/simple/hello/controller/HelloController.java
@@ -1,16 +1,26 @@
 package sc.simple.hello.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("hello")
 public class HelloController {
 
+    @Value("${mysqldb.datasource.url}")
+    private String username;
+
+    public void setUsername(String name){
+        this.username = name;
+    }
+
     @GetMapping("get")
     public String get(){
-        return "hello";
+        return username;
     }
 
 }
