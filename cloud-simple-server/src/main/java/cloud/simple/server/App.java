407ade58ed134489133a0241ce0bfed4fa531f13
ZHQ <hq.zhang@yunding360.com>
2018-06-19 13:46:19 +0800

    init

--- cloud-simple-server/src/main/java/cloud/simple/server/App.java
+++ cloud-simple-server/src/main/java/cloud/simple/server/App.java
@@ -1,10 +1,15 @@
 package cloud.simple.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-public class App 
+@EnableDiscoveryClient
+@SpringBootApplication
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class);
     }
 }
